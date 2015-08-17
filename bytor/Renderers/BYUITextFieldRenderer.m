d9e137064794d01d5553380ded5849c9f4d2b001
Eric <eric.lee.edl@gmail.com>
2015-08-16 18:31:15 -0700

    Added type checking for UITextField

--- bytor/Renderers/BYUITextFieldRenderer.m
+++ bytor/Renderers/BYUITextFieldRenderer.m
@@ -12,33 +12,37 @@
 @implementation BYUITextFieldRenderer
 
 -(void) renderView:(UITextField *)view withStyle:(BYStyle *)style {
-    [self renderProperty: @"font" withStyle: style withOperation:^(id value) {
+    NSArray *stringTypes = @[[NSString class]];
+    NSArray *numberTypes = @[[NSNumber class]];
+    NSArray *colorTypes = @[[UIColor class]];
+    
+    [self renderProperty: @"font" withStyle: style withTypeClasses: stringTypes withOperation:^(id value) {
         UIFont *newFont = [UIFont fontWithName: value size: view.font.pointSize];
         view.font = newFont;
     }];
     
-    [self renderProperty: @"text-size" withStyle: style withOperation:^(id value) {
+    [self renderProperty: @"text-size" withStyle: style withTypeClasses: numberTypes withOperation:^(id value) {
         UIFont *newFont = [UIFont fontWithName: view.font.fontName size: [value floatValue]];
         view.font = newFont;
     }];
     
-    [self renderProperty: @"min-text-size" withStyle: style withOperation:^(id value) {
+    [self renderProperty: @"min-text-size" withStyle: style withTypeClasses: numberTypes withOperation:^(id value) {
         view.minimumFontSize = [value floatValue];
     }];
     
-    [self renderProperty: @"text-color" withStyle: style withOperation:^(id value) {
+    [self renderProperty: @"text-color" withStyle: style withTypeClasses: colorTypes withOperation:^(id value) {
         view.textColor = value;
     }];
     
-    [self renderProperty: @"text-alignment" withStyle: style withOperation:^(id value) {
+    [self renderProperty: @"text-alignment" withStyle: style withTypeClasses: stringTypes withOperation:^(id value) {
         view.textAlignment = [BYRendererUtility textAlignmentForString: value];
     }];
     
-    [self renderProperty: @"border-style" withStyle: style withOperation:^(id value) {
+    [self renderProperty: @"border-style" withStyle: style withTypeClasses: stringTypes withOperation:^(id value) {
         view.borderStyle = [BYRendererUtility textBorderStyleForString: value];
     }];
     
-    [self renderProperty: @"background-image" withStyle: style withOperation:^(id value) {
+    [self renderProperty: @"background-image" withStyle: style withTypeClasses: stringTypes withOperation:^(id value) {
         view.background = [UIImage imageNamed: value];
     }];
     
